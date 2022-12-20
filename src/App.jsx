3de0f60ea82f9a962627140cb4396d73e20a71c6
karen1278 <kjaneth111@gmail.com>
2022-12-20 17:56:27 -0500

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,23 @@ function App() {
               <h4>Desarroladora web frontend</h4>
               <button>Contactar</button>
             </div>
-            <img className='photo' src="../photo.png" alt="carita" />
+            <img className='photo' src="../img/photo.png" alt="carita" />
           </header>
           <section>
             <h3 className='text-start '>Habilities</h3>
             <div className='d-flex justify-content-evenly'>
-              <img src="../html.png" alt="nn" />
-              <img src="../css.png" alt="nn" />
-              <img src="../js.png" alt="nn" />
-              <img src="../react.png" alt="nn" />
+              <img src="../img/html.png" alt="nn" />
+              <img src="../img/css.png" alt="nn" />
+              <img src="../img/js.png" alt="nn" />
+              <img src="../img/react.png" alt="nn" />
             </div>
           </section>
           <section>
             <h3>Proyects</h3>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img src="../img/webtributo.jpg" alt="" />
+            <img src="../img/rickandmorty.jpg" alt="" />
+            <img src="../img/wheelofdoom.jpg" alt="" />
+            <img src="../img/calculadora.jpg" alt="" />
           </section>
           <section>
             <h3>Experiencia</h3>
@@ -48,9 +48,9 @@ function App() {
           <section>
             <h3>Contacto</h3>
             <div className='d-flex justify-content-around'>
-              <img src="../linkedin" alt="x" />
-              <img src="../whatsapp" alt="x" />
-              <img src="../gihub" alt="x" />
+              <img src="../img/linkedin.png" alt="x" />
+              <img src="../img/whatsapp.png" alt="x" />
+              <img src="../img/github.png" alt="x" />
             </div>
           </section>
           
